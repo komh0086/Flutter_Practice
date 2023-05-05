de9335502b1910afeb2f88fb79c8c62d877ce2db
고민혁 <rhalsgur0086@gmail.com>
2023-05-05 21:40:21 +0900

    edit http_Practice

--- practice/Http_Practice/http_practice/lib/Screen/Screen_Main.dart
+++ practice/Http_Practice/http_practice/lib/Screen/Screen_Main.dart
@@ -1,23 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+
+import '../model/Person.dart';
 
 class ScreenMain extends StatelessWidget{
 
 
   @override
   Widget build(BuildContext context){
-    final response = getTacos();
-    print(response);
+    //이런느낌으로 통신하고
+    var jsonString = http.get(Uri.parse('http://www.youtube.com')).toString();
+    //이렇게 맵으로 바꾸고
+    Map<String, dynamic> userMap = jsonDecode(jsonString);
+    //이걸로 객체로 매핑하고
+    var user = Person.fromJson(userMap);
+    //이건 객체를 json으로 바꾸는거고
+    String json = jsonEncode(user);
+
     return Scaffold(
       body: Center(
         child: Text('asdf'),
       ),
     );
-  }
-
-  Future<http.Response> getTacos(){
-    return http.get(Uri.parse('localhost:8080/design/recent'));
   }
 
 }
